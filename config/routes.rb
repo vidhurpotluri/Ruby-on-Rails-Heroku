c5197d500237feab6cf939eff6f59d156ec3206c
vidhur-potluri <vidhur.potluri@tamu.edu>
2020-09-18 21:30:21 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'books/destroy'
   get 'books/delete'
   resources :books
+  root to:'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
